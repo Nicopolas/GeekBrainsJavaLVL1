66ca507d0e9f5111e1b6cc00882f9e4147b99093
Zaharov Nikolay <slepdog@gmail.com>
2018-12-16 14:57:39 +0400

    lesson's 5 homework

--- src/GeekBrainsJavaLVL1/Main.java
+++ src/GeekBrainsJavaLVL1/Main.java
@@ -4,5 +4,18 @@ package GeekBrainsJavaLVL1;
  * Created by 1 on 09.12.2018.
  */
 public class Main {
+    public static void main(String[] args) {
+        Person[] persArray = new Person[5];
+        persArray[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30);
+        persArray[1] = new Person("Petrov Petr", "Engineer", "petrov@mailbox.com", "890912312", 30000, 44);
+        persArray[2] = new Person("Sidorov Sergei", "Engineer", "sidorov@mailbox.com", "890312312", 30000, 41);
+        persArray[3] = new Person("Smirnov Ivan", "Engineer", "smirnov@mailbox.com", "898512312", 30000, 29);
+        persArray[4] = new Person("Volkov Vladimir", "Engineer", "volkov@mailbox.com", "890712312", 30000, 25);
 
+        for (Person person : persArray) {
+            if (person.getAge() > 40) {
+                person.displaysInformation();
+            }
+        }
+    }
 }

3686bc98c908caebd796f38ea7affc97f30700c6
Zaharov Nikolay <slepdog@gmail.com>
2018-12-23 20:38:06 +0400

    master commit

--- src/GeekBrainsJavaLVL1/Main.java
+++ src/GeekBrainsJavaLVL1/Main.java
@@ -5,16 +5,5 @@ package GeekBrainsJavaLVL1;
  */
 public class Main {
     public static void main(String[] args) {
-        Cat[] arrCat = {new Cat("Barsik", 5),
-                        new Cat("Vaska", 10),
-                        new Cat("Lucky", 10),
-                        new Cat("Kot", 10),
-                        new Cat("Jora", 100),};
-        Plate plate = new Plate(100);
-
-        for (Cat cat : arrCat) {
-            cat.eat(plate);
-            cat.info();
-        }
     }
 }
